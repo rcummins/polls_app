65e2070cede27930be45521a149eec9f6db7c36d
Renata Cummins <renatacummins@gmail.com>
2019-06-13 10:31:39 -0400

    Add Question#results to give counts of responses

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,19 @@ class Question < ApplicationRecord
     has_many :answer_choices
     belongs_to :poll
     has_many :responses, through: :answer_choices
+
+    def results
+        response_data = Question
+            .select('answer_choices.text, COUNT(responses.id) AS counts')
+            .left_outer_joins(:responses)
+            .where('questions.id = ?', self.id)
+            .group('answer_choices.text')
+
+        response_counts = {}
+        response_data.each do |answer_choice|
+            response_counts[answer_choice.text] = answer_choice.counts
+        end
+
+        response_counts
+    end
 end
